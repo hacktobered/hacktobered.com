026c8fe076aed2fd7e811557b91a6f56c49c4615
Ankit Kashyap <ankitwww@gmail.com>
2022-10-24 18:50:55 -0700

    Fixed page wrapper

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -6,7 +6,7 @@ import { NavBar } from "./NavBar";
 export const PageWrapper = (props: BoxProps) => (
   <Box minH="100vh" bg="gray.100">
     <NavBar />
-    <Box maxW="7xl" mx="auto" px={{ base: "6", lg: "8" }} {...props}></Box>
+    <Box {...props}></Box>
     <Footer />
   </Box>
 );
