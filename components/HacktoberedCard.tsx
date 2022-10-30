6e603de681b60b14424f006462d92e29d0670e34
nishtha53 <68805896+nishtha53@users.noreply.github.com>
2022-10-31 01:37:16 +0530

    fix: solve file issue

--- components/HacktoberedCard.tsx
+++ components/HacktoberedCard.tsx
@@ -19,6 +19,7 @@ import download from "downloadjs";
 import { toPng } from "html-to-image";
 import { useRef } from "react";
 
+
 type HacktoberedCardType = {
   user: UserDetails;
   prCount: number;
@@ -59,13 +60,14 @@ const HacktoberedCard = (props: HacktoberedCardType) => {
             position="relative"
             bg={useColorModeValue("white", "gray.700")}
             shadow={{ md: "base" }}
+            style={{"display":"flex"}}
           >
             <Box
               position="absolute"
               inset="0"
-              height="24"
               bg="blue.600"
               roundedTop="inherit"
+              h={8}
             />
             <Avatar size="2xl" src={user.avatar_url}></Avatar>
             <VStack spacing="1" flex="1">
